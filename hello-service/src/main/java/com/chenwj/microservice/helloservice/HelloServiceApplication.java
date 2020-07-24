249616c2112a741f5b1ecd08584b1e1538cfbb31
chenweijie <chenweijie@sunlands.com>
2020-07-24 19:13:18 +0800

    修改helloserver

--- hello-service/src/main/java/com/chenwj/microservice/helloservice/HelloServiceApplication.java
+++ hello-service/src/main/java/com/chenwj/microservice/helloservice/HelloServiceApplication.java
@@ -2,10 +2,9 @@ package com.chenwj.microservice.helloservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
 public class HelloServiceApplication {
 
     public static void main(String[] args) {
